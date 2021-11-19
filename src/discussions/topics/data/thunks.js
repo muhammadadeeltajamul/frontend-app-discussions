fe05e4dec739520363f2d5bf07635020b5bd1385
Kshitij Sobti <kshitij@sobti.in>
2021-11-19 11:12:48 +0530

    fix: thread counts are now displayed again on the topic listing page

--- src/discussions/topics/data/thunks.js
+++ src/discussions/topics/data/thunks.js
@@ -11,13 +11,15 @@ function normaliseTopics(data) {
   const categoryIds = [];
   data.coursewareTopics.forEach(category => {
     topicsInCategory[category.name] = category.children.map(topic => {
-      topics[topic.id] = { id: topic.id, name: topic.name, categoryId: category.name };
+      topics[topic.id] = {
+        categoryId: category.name, ...topic,
+      };
       return topic.id;
     });
     categoryIds.push(category.name);
   });
   const nonCoursewareIds = data.nonCoursewareTopics.map(topic => {
-    topics[topic.id] = { id: topic.id, name: topic.name };
+    topics[topic.id] = topic;
     return topic.id;
   });
   return {
